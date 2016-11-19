ecd5cad08ed4b9f8b27c26426f2748444122da10
cnc-expert <cnc-expert@mail.ru>
2016-11-19 10:29:51 +0500

    Symbol table bugfix.
    	изменено:      src/main.c
    	изменено:      src/main.h

--- src/main.h
+++ src/main.h
@@ -1,7 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void yyerror (const char *s);
 char* AddToSymbolTable(char* yytext);
 
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif // MAIN_H
+
